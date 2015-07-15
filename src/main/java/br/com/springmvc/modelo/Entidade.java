1211d84917b1880f2fc083979bcac68519b78cbf
franklindeoliveira <franklindeoliveira@gmail.com>
2015-07-15 12:44:07 -0300

    Versão simplificada do repositório 'springmvc-jpa', utilizando apenas o banco de dados postgres.

---
+++ src/main/java/br/com/springmvc/modelo/Entidade.java
@@ -0,0 +1,43 @@
+package br.com.springmvc.modelo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "entidade")
+public class Entidade {
+
+	@Id
+	@GeneratedValue
+	private Integer id;
+	
+	private String atributo;
+	
+	public Entidade(String atributo) {
+		this.atributo = atributo;
+	}
+	
+	public Entidade(Integer id, String atributo) {
+		this.id = id;
+		this.atributo = atributo;
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getAtributo() {
+		return atributo;
+	}
+
+	public void setAtributo(String atributo) {
+		this.atributo = atributo;
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,9 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt" %>
+<html>
+	<body>
+		<h2>Spring MVC!</h2>
+		<ul>
+			<li><a href="http://localhost:8080/springmvc/repository">GO</a> - Exemplo de acesso ao banco de dados utilizando injeção de dependência.</li>
+		</ul>
+	</body>
+</html>
